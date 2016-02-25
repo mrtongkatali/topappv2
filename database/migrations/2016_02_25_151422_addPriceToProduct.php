c676f4c7e08f5f91c5963cb96b76d24947539de0
mrtongkatali <leoangelo.diaz@gmail.com>
2016-02-26 01:12:56 +0800

    add base_price field to product

---
+++ database/migrations/2016_02_25_151422_addPriceToProduct.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPriceToProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('base_price',5,2)->before('status');;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('base_price');
+        });
+    }
+}
